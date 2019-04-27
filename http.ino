abf1a61bf845a5e75d94d5c84150329da45b58e9
cr4fun <38368200+cr4fun@users.noreply.github.com>
2019-04-27 21:18:58 +0800

    Update http.ino

--- http.ino
+++ http.ino
@@ -9,42 +9,69 @@
 #include "AudioFileSourceBuffer.h"
 #include "AudioGeneratorMP3.h"
 #include "AudioOutputI2SNoDAC.h"
-
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <string.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 // To run, set your ESP8266 build to 160MHz, update the SSID info, and upload.
-
 // Enter your WiFi setup here:
 const char *SSID = "hashfish";
 const char *PASSWORD = "56cf12d8";
-
 // Randomly picked URL
-
-
 AudioGeneratorMP3 *mp3;
 AudioFileSourceICYStream *file;
 AudioFileSourceBuffer *buff;
 AudioOutputI2SNoDAC *out;
-
 ESP8266WebServer server(80);
 String pm3file = "";
-String token = "24.3723a9f93797598f4fb6163e81c52955.2592111.1558893321.282335-15738833";
+String token = "";
+void gettoken() {
+  WiFiClient client;
+  HTTPClient http;
+  String value = server.arg(0);
+  if (http.begin(client, "http://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=18PUVnFTdVn49bdMuBNr0sZc&client_secret=jU04KneI35AFKFpgjvaPTYEZE15sQTUH")) {  // HTTP
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();
+        payload = payload.substring(payload.indexOf("access_token") + 15);
+        token = payload.substring(0, payload.indexOf("session_key") - 3);
+        Serial.println(token);
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  } else {
+    Serial.printf("[HTTP} Unable to connect\n");
+  }
+}
 void setup()
 {
 
   Serial.begin(115200);
-  delay(1000);
   //  WiFi.disconnect();
   //WiFi.softAPdisconnect(true);
+  Serial.println("setting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, PASSWORD);
-  // Try forever
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("...Connecting to WiFi");
+    Serial.printf("conn...\n");
     delay(1000);
   }
   Serial.println(WiFi.localIP());
+  gettoken();
+
   server.on("/", []() {
     String html = "";
     html += "<html><body>\n";  //此部分为浏览器上显示的信息，可根据实际修改美化
@@ -68,10 +95,7 @@ void setup()
       if (!mp3->loop()) mp3->stop();
     }
   });
-  server.on("/test", []() {
-    String value = server.arg(0);
-    server.send(200, "text/plain", "this works as well");
-  });
+
   server.on("/say", []() {
     String text = server.arg(0);
     String url = "http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=abcdxxx&tok=";
@@ -79,7 +103,7 @@ void setup()
     url.concat("&tex=%20%20");
     url.concat(text);
     url.concat("%20%20%20%20%20%20");
-    url.concat("&vol=9&per=0&spd=5&pit=5&aue=3");
+    url.concat("&vol=15&per=0&spd=5&pit=5&aue=3");
     Serial.println(url);
     const char *u = url.c_str();
     file = new AudioFileSourceICYStream(u);
@@ -90,6 +114,7 @@ void setup()
     server.send(200, "text/plain", "this works as well");
     while (mp3->isRunning()) {
       if (!mp3->loop()) {
+        delay(1000);
         mp3->stop();
       };
     }
@@ -100,10 +125,5 @@ void setup()
 
 void loop()
 {
-
   server.handleClient();
-  if (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    WiFi.begin(SSID, PASSWORD);
-  }
 }
